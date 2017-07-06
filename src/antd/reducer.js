52185faf9febaaf3d1c764af7acecd3f74ab879f
lcx <lcx@qq.com>
2017-07-06 14:07:11 +0800

    login localStorage

--- src/antd/reducer.js
+++ src/antd/reducer.js
@@ -1,5 +1,6 @@
-import {get} from './request'
+// import {get} from './request'
 
+// 记录action带来的状态变化
 
 const Login = (state = {
     mobile:'',
@@ -8,31 +9,27 @@ const Login = (state = {
 } , action) =>{
     switch(action.type){
         case 'GET_MOBILE':
-            return {mobile:action.mobile,password:action.password}
+            return {...state, mobile:action.mobile}
         case 'GET_PASSWORD':
-            return {mobile:action.mobile,password:action.password}
-        case 'LOGIN':
-            // if(state.msg)
-            return
-        case 'GET_SUCCED':
-            if(state.mobile!=="" && state.password!==""){
-                if(state.mobile==action.mobile){
-                    if(state.password==action.password){
-                        return {msg:'登录成功'}
-                    }else{
-                        return {msg:'用户名或密码不正确'}
-                    }
-                }else{
-                    return {msg:'用户不存在'}
-                }                
-            }else if(state.mobile==""){
-                return {msg:'请输入手机号'}
-            }else{
-                return {msg:'请输入密码'}               
-            }
+            return {...state, password:action.password}
+        case 'MSG':
+            return {...state,msg:action.msg}
         default:
             return state
     }
 }
 
 export default Login;
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/antd/sagas.js
+++ src/antd/sagas.js
@@ -1,20 +1,66 @@
 import { take, put, call, select } from 'redux-saga/effects'
 import { get } from './request'
 
-function* login() {
-    while(true){
-        const mobile = yield take('GET_MOBILE')
-        const users = yield call(get)
-        const result = users.filter(user => user.mobile == mobile.mobile)
+//进行相应动作逻辑判断
 
-        if(mobile.mobile == result.mobile){
-            const pwd = yield take('GET_PASSWORD')
-            if(pwd.password == result.password){
-                console.log(result)
-                yield put({type:'GET_SUCCED',data:result})            
+// function* mobile() {
+//     while(true){
+//         const mobile = yield take('GET_MOBILE')
+//         const result = yield select(state => state)
+//     }
+// }
+
+// function* pwd() {
+//     while(true){
+//         const pwd = yield take('GET_PASSWORD')
+//         const result = yield select(state => state)
+//     }
+// }
+
+function* succed() {
+    while(true){
+        const data = yield take('GET_SUCCED') //action 不改变state
+        // console.log(data)
+        const {mobile, password} = yield select(state => state) //getState {mobile: "", password: "", msg: ""}
+        console.log(mobile,password)
+        if(mobile!=="" && password!==""){ 
+            const result = yield call(get)
+            const user = result.find(user => mobile===user.mobile) //Array->object  filter->Array->Array
+            console.log(user)
+            if(user!==undefined){
+                if(mobile===user.mobile){
+                    if(password === user.password){
+                        yield put({type:'MSG',msg:'登录成功'})
+                    }else{
+                        yield put({type:'MSG',msg:'密码不正确'})
+                    }
+                }
+            }else{
+                yield put({type:'MSG',msg:'用户不存在'})
             }
+        }else if(mobile===""){
+            yield put({type:'MSG',msg:'请输入手机号'})
+        }else{
+            yield put({type:'MSG',msg:'请输入密码'}) //派发action state改变
         }
+
     }
 }
 
-export default [login]
+
+export default [succed]
+
+
+        // const users = yield call(get)
+        // const result = users.filter(user => user.mobile == mobile.mobile)
+
+        // if(mobile.mobile == result.mobile){
+        //     const pwd = yield take('GET_PASSWORD')
+        //     if(pwd.password == result.password){
+        //         console.log(result)
+        //         yield call()         
+        //     }
+        // }
+
+
+
